8db19153fe7514d59696e82700a134e3820c2d67
zeiv <fxb9500@gmail.com>
2014-01-09 19:25:08 -0600

    Pretty print JSON

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
+    render json: JSON.pretty_generate(JSON.parse(@article.to_json))
   end
 
   # GET /articles/new
